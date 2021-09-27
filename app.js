1e196713b8b1956dc676deebc80ebf4b8a06cbb0
DWitek97 <57908461+DWitek97@users.noreply.github.com>
2021-09-27 21:03:08 +0200

    test

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 //array holding all guest names and number of times they visited
 var name_list = [{firstName:"Elisabeth", count: 1},
-                 {firstName:"David", count: 12},
-                 {firstName:"Axel", count: 4},
-                 {firstName:"Sebastian", count: 2}];
+                {firstName:"David", count: 12},
+                {firstName:"Axel", count: 4},
+              {firstName:"Sebastian", count: 2}];
 
 function addName(){
     //Form input
     var new_name = document.getElementById("fname").value.toString();
     //console.log("New name = " + new_name);
-    
+
     //index of array entry if name already in list
     // -1 if not in list yet
     var list_flag = isInList(new_name);
-    
+
     //if already in list, increase counter
     //else add to list
     if (list_flag != (-1)) {
         //console.log("Is already in list");
         name_list[list_flag].count += 1;
     } else {
-        //console.log("New name");    
+        //console.log("New name");
         var new_object = {firstName: new_name, count: 1}
-        name_list.push(new_object);        
-    }    
+        name_list.push(new_object);
+    }
     // print all guests to web page
     printToPage();
 }
 
-// check if name is already in list 
+// check if name is already in list
 // if yes, return index of array entry
 // else return -1
 function isInList(name) {
@@ -39,7 +39,7 @@ function isInList(name) {
     return -1;
 }
 
-// print list to web page 
+// print list to web page
 function printToPage(){
     var all_names = "";
     for(var i = 0; i < name_list.length; i++){
@@ -47,7 +47,7 @@ function printToPage(){
             all_names = name_list[i].firstName + " has visited " + name_list[i].count.toString() + " times.";
         } else {
             all_names = all_names + "<br>" + name_list[i].firstName + " has visited " + name_list[i].count.toString() + " times.";
-        }        
+        }
         //console.log(all_names);
         document.getElementById("guests").innerHTML = all_names;
     }
@@ -73,10 +73,7 @@ function myFunction(xhttp) {
 function addNamePHP() {
     var fname = document.getElementById("fname").value.toString();
     //console.log("First Name = " + fname);
-    if (fname.length == 0) {
-        document.getElementById("guests").innerHTML = "";
-    return;
-  } else {
+
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -84,9 +81,10 @@ function addNamePHP() {
           document.getElementById("guests").innerHTML = this.responseText;
       }
     };
-    xmlhttp.open("POST", "app.php?q=" + fname, true);
-    xmlhttp.send();
-  }
+    xmlhttp.open("POST", "app.php", true);
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send("fname=" + fname);
+
 }
 
 function printResults(res) {
--- app.php
+++ app.php
@@ -1,18 +1,53 @@
 <?php
     $myfile = fopen("data.json", "r") or die("Unable to open file!");
     $jsonobj = fread($myfile, filesize("data.json"));
-    # echo $jsonobj;
+    fclose($myfile);
+    $data_as_php = json_decode($jsonobj, true);
 
-    $data_as_php = json_decode($jsonobj);
     $res_str = "";
+    $isInArray = 0;
 
-    foreach($data_as_php as $key => $value) {
-        # echo $key . " => " . $value . "<br>";
-        $res_str .= $key . " has visited " . $value . " times.<br>";
+
+    if (array_key_exists('fname', $_POST)) {
+        $fname = $_POST['fname'];
+    } else {
+        $fname = "Kein Name";
     }
 
-    echo $res_str;
-    fclose($myfile);
+    if (strlen($fname) > 0) {
+
+        foreach($data_as_php as $key => $value) {
+            # echo $key . " => " . $value . "<br>";
+            if ($key == $fname) {
+                $isInArray = 1;
+                $data_as_php[$key]++;
+            }
+            $res_str .= $key . " has visited " . $value . " times.<br>";
+        }
+
+        if ($isInArray == 0) {
+            $data_as_php[$fname] = 1;
+        }
+
+        $myfile2 = fopen("data.json", "w") or die("Unable to open file!");
+        fwrite($myfile2, json_encode($data_as_php));
+        fclose($myfile2);
+
+        echo $res_str;
+
+    } else {
+    foreach($data_as_php as $key => $value) {
+        $res_str .= $key . " has visited " . $value . " times.<br>";
+        }
+        echo $res_str;
+    }
+
+
+
+
+
+
+
 
     # $new_entry = json_encode(array("Felix"=>9));
     # $new_entry = json_encode($data_as_php->append("Felix:9"));
