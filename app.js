9d9da16521e55936969889126de118a268e2b2b7
Lizzylizard <elisabeth.milde@informatik.hs-fulda.de>
2021-09-29 20:37:25 +0200

    bmi

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ function bmi(){
     var name = document.getElementById("fname").value.toString();
     console.log(name);
     // gewicht / groesse ^ 2 = bmi
-    var bmi = weight / (size * size);
+    var bmi = (weight / (size * size)).toFixed(2);
+
+    var res = "";
+    if (bmi < 19) {
+        res = "too low.";
+    } else if(bmi >=19 && bmi < 25) {
+        res = "normal.";
+    } else {
+        res = "too high.";
+    }
 
     document.getElementById("res").innerHTML = bmi;
     document.getElementById("name").innerHTML = "Hello " + name + ", your BMI is:";
+    document.getElementById("bewertung").innerHTML = "Your bmi is " + res;
+
 }
